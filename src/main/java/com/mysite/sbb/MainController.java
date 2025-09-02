30befa8ff19c14efae21fb07e3bd0f10558f1d77
MinseoKim01 <202513341@pukyong.ac.kr>
2025-09-02 15:08:01 +0900

    JPA 데이터베이스 사용 준비하기

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// MainController 클래스가 스프링 부트의 컨트롤러가 되는 것 
+@Controller
+public class MainController {
+	@GetMapping("/sbb")
+	@ResponseBody
+	public String index() {
+		return "안녕하세요. SBB에 오신 것을 환영합니다.";
+	}
+}
