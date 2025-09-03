ed31fda02b5e03f41b92218744e29f296d38f540
MinseoKim01 <202513341@pukyong.ac.kr>
2025-09-03 12:35:33 +0900

    질문 데이터 수정하기

--- src/test/java/com/example/demo/SbbApplicationTests.java
+++ src/test/java/com/example/demo/SbbApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,9 +23,11 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {        
-    	List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-    	Question q = qList.get(0);
-    	assertEquals("sbb가 무엇인가요?", q.getSubject());
+    	Optional<Question> oq = this.questionRepository.findById(1);
+    	assertTrue(oq.isPresent());
+    	Question q = oq.get();
+    	q.setSubject("수정된 제목");
+    	this.questionRepository.save(q);
     	}
 }
 
